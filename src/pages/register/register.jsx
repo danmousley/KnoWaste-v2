20bd3c95103149c2f44d618d656ed5576783aacf
danmousley <dannymousley@outlook.com>
2021-09-13 15:11:10 +0700

    Added step state and step titles.

--- src/pages/register/register.jsx
+++ src/pages/register/register.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Register = () => {
+    const [formStep, setFormStep] = useState("step1");
+    const name = "name placeholder" // insert name here once we have access to step one of form
+    const getTitle = () => {
+        if (formStep === "step1") {
+            return "Let's get started..."
+        } else if (formStep === "step2") {
+            return `Nice to meet you, ${name}!! 👋`
+        } else if (formStep === "step3") {
+            return `Final step...`
+        }
+    }
     return (
-        <div>
-            Register page under construction
-        </div>
+        <>
+            <header>
+                <h2> { getTitle() } </h2>
+            </header>
+            <section>
+                <nav>
+                    Nav section
+                </nav>
+                <div>
+                    Form section
+                </div>
+            </section>
+        </>
     )
 }
 
