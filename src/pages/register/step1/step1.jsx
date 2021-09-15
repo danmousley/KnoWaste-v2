27ff7fc9c28ad6ca56bd9310857ff45bcd32aab1
danmousley <dannymousley@outlook.com>
2021-09-15 14:56:04 +0700

    Next button disabled when form invalid.

--- src/pages/register/step1/step1.jsx
+++ src/pages/register/step1/step1.jsx
@@ -1,23 +1,32 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import styles from './step1.scss'
 import { useForm } from "react-hook-form";
 
 const Step1 = (props) => {
     const {onSubmit} = props;
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
-    // const onSubmit = (data) => { //how do I move this to register?
-    //     console.log(data)
-    // }
+    const step1Form = useRef(null)
+    const step1Submit = useRef(null)
+
+    const checkFormIsValid = (e) => {
+        console.log(e.target.value)
+        if (step1Form.current.checkValidity()) {
+            step1Submit.current.disabled = false
+            step1Form.current.classList.add('was-validated')
+        }
+    }
 
     return (
         <>
-            <form onSubmit={handleSubmit(onSubmit)}>
+            <form class="needs-validation" ref={step1Form} onSubmit={handleSubmit(onSubmit)} novalidate>
                 <section class="container form">
                     <div class="row form__row">
                         <div class="col">
                             <h6 class="form__label">First Name</h6>
-                            <input {...register("firstName", { required: true })} type="text" className={`form__input ${errors.firstName ? "invalid" : ""} `}/>
-                            {errors.firstName && <span class="form__error-message">This field is required</span>}
+                            <input {...register("firstName")} type="text" onBlur={(e) => checkFormIsValid(e)} class="form-control form__input" required/>
+                            <div class="invalid-feedback">
+                                Please enter a valid first name
+                            </div>
                         </div>
                         <div class="col">
                             <h6 class="form__label">Last Name</h6>
@@ -51,7 +60,7 @@ const Step1 = (props) => {
                 <div class="buttons container">
                     <div class="row h-100">
                         <div class="col my-auto">
-                            <button type="submit" class="btn btn-primary button--form" >Next</button>
+                            <button type="submit" ref={step1Submit} class="btn btn-primary button--form" disabled aria-disabled="true">Next</button>
                         </div>
                     </div>
                 </div>
