9f26e208d76172b78ee1939888208995f5c3780d
danmousley <dannymousley@outlook.com>
2021-09-16 12:53:20 +0700

    Consolidated stepStyles to a single function

--- src/pages/register/stepper/stepper.jsx
+++ src/pages/register/stepper/stepper.jsx
@@ -4,51 +4,32 @@ import styles from './stepper.scss';
 const Stepper = (props) => {
     const {formStep, handleStepper} = props;
 
-    const step1Styles = () => {
-        if (formStep === 1) {
-            return "active"
-        } else {
-            return "complete"
-        }
-    }
-    
-    const step2Styles = () => {
-        if (formStep === 1) {
+    const stepStyles = (step) => {
+        if (formStep < step) {
             return "default"
-        } else if (formStep === 2) {
+        } else if (formStep === step) {
             return "active"
-        } else {
+        } else if (formStep > step) {
             return "complete"
         }
     }
 
-    const step3Styles = () => {
-        if (formStep === 1) {
-            return "default"
-        } else if (formStep === 2) {
-            return "default"
-        } else {
-            return "active"
-        }
-    }
-
-    
     return (
         <nav class="container stepper h-100">
             <div class="row stepper__row h-100">
-                <div className={`col step ${step1Styles()}`} >
+                <div className={`col step ${stepStyles(1)}`} >
                     <button value={1} type="button" onClick={(e) => handleStepper(e)} class="stepper__button">
                         <h5 class="step-number">STEP 01</h5>
                         <span class="step-name">Personal Details</span>
                     </button>
                 </div>
-                <div className={`col step ${step2Styles()}`} >
+                <div className={`col step ${stepStyles(2)}`} >
                     <button value={2} onClick={(e) => handleStepper(e)} class="stepper__button">
                         <h5 class="step-number">STEP 02</h5>
                         <span class="step-name">User Details</span>
                     </button>
                 </div>
-                <div className={`col step ${step3Styles()}`} >
+                <div className={`col step ${stepStyles(3)}`} >
                     <button value={3} onClick={(e) => handleStepper(e)} class="stepper__button">
                         <h5 class="step-number">STEP 03</h5>
                         <span class="step-name">Terms & Conditions</span>
