6322c5430dca63106064202ab254fcc4bd7a120d
Katie Walker <katiewalker0517@gmail.com>
2021-09-22 13:11:38 +0100

    added mobile version and more styling

--- src/shared/navbar/navbar.jsx
+++ src/shared/navbar/navbar.jsx
@@ -1,22 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './navbar.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import logo from '../../assets/images/logo.png';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 const NavBar = () => {
-  return (
-    <nav class='nav d-flex justify-content-between align-items-center p-3'>
-      <div class='nav__title d-flex align-items-center ms-3'>
-        <img src={logo}/>
-        <h1>Knowaste</h1>
-      </div>
-      <ul class='nav__links  d-flex justify-content-around align-items-center me-3'>
-        <li class='px-3 nav__link'><a href=''>How it works?</a></li>
-        <li class='px-3 nav__link'><a href=''>Admin</a></li>
-        <li class='px-3 nav__link'><a href='' class='btn btn-primary'>Login</a></li>
-      </ul>
+  
+  const [ isNavOpen, setIsNavOpen ] = useState(false);
 
+  const handleClick = () => {
+    setIsNavOpen(!isNavOpen);
+  }
+
+  const displayMobileNav = isNavOpen ? "mobile-visible" : "";
+  
+  return (
+    <nav className={`nav ${displayMobileNav}`}>
+      <div className='nav__title'>
+        <div className='logo'>
+          <img className='logo__img' src={logo}/>
+          <h1>Knowaste</h1>
+        </div>
+        <div className='nav__icon-container'>
+          <FontAwesomeIcon className='nav__close nav__icon' icon={faTimes} size='2x' onClick={handleClick}/>
+        </div>
+      </div>
+      <ul className='nav__links'>
+        <li className='nav__link'><a href=''>How it works?</a></li>
+        <li className='nav__link'><a href=''>Admin</a></li>
+        <li className='nav__link'><a href='' class='btn btn-primary'>Login</a></li>
+      </ul>
+      <div className='nav__icon-container'>
+        <FontAwesomeIcon className='nav__hamburger nav__icon' icon={faBars} size='3x' onClick={handleClick}/>
+      </div>
     </nav>
+
   )
 }
 
